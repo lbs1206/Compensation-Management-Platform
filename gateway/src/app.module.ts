dd4956223e5daa42aa4c8715fd7e1124ca28d9ba
benson <qudtjr1993@nate.com>
2025-05-17 17:22:39 +0900

    [Gateway] gateway init

---
+++ gateway/src/app.module.ts
@@ -0,0 +1,21 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+  ],
+})
+export class AppModule {}
